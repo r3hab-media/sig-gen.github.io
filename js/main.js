f7bd5bbba5072da950b40342183fa2887809bdc2
Jared Newnam <jnewnam@scottsdaleaz.gov>
2023-10-19 13:13:45 -0700

    many updates

--- js/main.js
+++ js/main.js
@@ -1,79 +1,119 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const form = document.getElementById("signature--form");
+  // Hide the social media section on load
+  document.querySelector("#socialMediaSection").style.display = "none";
 
-	// Real-time update when any input changes
-	form.addEventListener("input", function (e) {
-		generateSignatures();
-	});
+  //capture signature generator button
+  const generateButton = document.getElementById("generate-button");
 
-	function generateSignatures() {
-		const formData = {
-			name: document.getElementById("name").value,
-			title: document.getElementById("title").value,
-			officePhone: document.getElementById("officePhone").value,
-			mobilePhone: document.getElementById("mobilePhone").value,
-			email: document.getElementById("email").value,
-			street: document.getElementById("street").value,
-			deptUrl: document.getElementById("deptUrl").value,
-			urlDisplayName: document.getElementById("urlDisplayName").value,
-			facebook: document.getElementById("facebook").value,
-			twitter: document.getElementById("twitter").value,
-			instagram: document.getElementById("instagram").value,
-			youtube: document.getElementById("youtube").value,
-		};
+  // Event Listener for Generate Button
+  generateButton.addEventListener("click", function () {
+    generateSignatures();
+  });
 
-		// Template 1: Basic
-		const template1 = `
-      <div class="signature--template">
-        <span class="name">${formData.name}</span>
-        <span class="title">${formData.title} | City of Scottsdale</span>
-        <span class="o-phone">${formData.officePhone}</span>
-        <span class="m-phone">${formData.mobilePhone}</span>
-        <span class="email">${formData.email ? `<a href="mailto:${formData.email}">${formData.email}</a>` : ""}
-        <span class="dept">
-          <a href="${formData.deptUrl || "https://scottsdaleaz.gov"}">${formData.urlDisplayName || "ScottsdaleAZ.gov"}</a>
-        </span>
-      </div>
-    `;
+  // Event Listeners for social media choices
+  document
+    .getElementById("socialMediaChoice1")
+    .addEventListener("change", toggleSocialSection);
+  document
+    .getElementById("socialMediaChoice2")
+    .addEventListener("change", toggleSocialSection);
 
-		// Template 2: Social Media on the Side
-		const template2 = `
-      <div class="signature-template">
-        <div>
-          <strong>${formData.name}</strong>
-          ${formData.title} | City of Scottsdale
-          ${formData.officePhone}
-          ${formData.email ? `<a href="mailto:${formData.email}">${formData.email}</a>` : ""}
+  function toggleSocialSection(event) {
+    const socialMediaSection = document.querySelector("#socialMediaSection");
+    socialMediaSection.style.display =
+      event.target.value === "socialMediaDefault" ? "none" : "flex";
+  }
+
+  function generateSignatures() {
+    //social links
+    const socialChoice = document.querySelector(
+      'input[name="socialMediaChoice"]:checked'
+    ).value;
+    // Gather all the input values
+    const name = document.getElementById("name").value;
+    const pnLetters = document.getElementById("pnLetters").value;
+    const pronoun = document.getElementById("pronoun").value;
+    const title = document.getElementById("title").value;
+    const officePhone = document.getElementById("officePhone").value;
+    const mobilePhone = document.getElementById("mobilePhone").value;
+    const deptName = document.getElementById("deptName").value;
+    const street = document.getElementById("street").value;
+    const zipCode = document.getElementById("zipCode").value;
+    const deptUrl =
+      document.getElementById("deptUrl").value || "https://scottsdaleaz.gov";
+    const deptUrlDisplay =
+      document.getElementById("deptUrlDisplay").value || "City of Scottsdale";
+    const facebook = document.getElementById("facebook").value;
+    const twitter = document.getElementById("twitter").value;
+    const instagram = document.getElementById("instagram").value;
+    const youtube = document.getElementById("youtube").value;
+
+    let socialLinks = "";
+    if (socialChoice === "socialMediaDefault") {
+      socialLinks = `<a href="https://www.scottsdaleaz.gov/news/social-media">Connect with us on social media</a>`;
+    } else {
+      if (facebook)
+        socialLinks += `<a href="${facebook}" class="sig__social--elem">Facebook</a>`;
+      if (twitter)
+        socialLinks += ` | <a href="${twitter}" class="sig__social--elem">Twitter</a>`;
+      if (instagram)
+        socialLinks += ` | <a href="${instagram}" class="sig__social--elem">Instagram</a>`;
+      if (youtube)
+        socialLinks += ` | <a href="${youtube}" class="sig__social--elem">YouTube</a>`;
+    }
+
+    let pnLettersTrue = pnLetters ? `<span>, ${pnLetters}</span>` : "";
+    
+    function formatPhoneNumber(phoneNumberString) {
+      let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+
+      // Remove leading '1' if present
+      if (cleaned.length === 11 && cleaned.startsWith('1')) {
+        cleaned = cleaned.substring(1);
+      }
+
+      const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+      if (match) {
+        return match[1] + '-' + match[2] + '-' + match[3];
+      }
+      return null;
+    }
+    
+    const formattedOfficePhone = formatPhoneNumber(officePhone);
+    const formattedMobilePhone = formatPhoneNumber(mobilePhone);
+    
+
+    const template3 = `
+      <div class="sig card">
+        <div class="card-body">
+          <div class="sig__name">${name}${pnLettersTrue}${
+      pronoun ? `<span class="sig__pronoun">(${pronoun})</span>` : ""
+    }</div>
+          ${title ? `<div class="sig__pos"> <span>${title} | City of Scottsdale</span></div>` : "" }
+          <div class="sig__phones">
+            ${formattedOfficePhone ? `<span class="sig__phones--p"><span>o</span> ${formattedOfficePhone}</span>` : "" }
+            ${formattedMobilePhone ? `<span class="sig__phones--m"><span>c</span> ${formattedMobilePhone}</span>` : "" }
+          </div>
+          ${
+            deptName && street && zipCode
+              ? `
+            <div class="sig__address">
+              <address>
+                <span class="sig__address--dept">${deptName}</span><br>
+                <span class="sig__address--street">${street}</span><br>
+                Scottsdale, AZ <span>${zipCode}</span>
+              </address>
+            </div>
+          `
+              : ""
+          }
+          <a href="${deptUrl}" class="sig__link">${deptUrlDisplay}</a>
+          <div class="sig__social">
+            ${socialLinks}
+          </div>        
         </div>
-        <div>
-          ${formData.facebook ? `<a href="${formData.facebook}">Facebook</a>` : ""}
-          ${formData.twitter ? `<a href="${formData.twitter}">Twitter</a>` : ""}
-          ${formData.instagram ? `<a href="${formData.instagram}">Instagram</a>` : ""}
-        </div>
       </div>
     `;
-
-		// Template 3: All-In with Social Media Icons
-		const template3 = `
-      <div class="signature-template">
-        <strong>${formData.name}</strong>
-        ${formData.title} | City of Scottsdale
-        ${formData.officePhone}
-        ${formData.email ? `<a href="mailto:${formData.email}">${formData.email}</a>` : ""}
-        <a href="${formData.deptUrl || "https://scottsdaleaz.gov"}">${formData.urlDisplayName || "ScottsdaleAZ.gov"}</a>
-        ${formData.facebook ? `<a href="${formData.facebook}">[FB]</a>` : ""}
-        ${formData.twitter ? `<a href="${formData.twitter}">[TW]</a>` : ""}
-        ${formData.instagram ? `<a href="${formData.instagram}">[IG]</a>` : ""}
-      </div>
-    `;
-
-		// Populate the real-time previews
-		// Using both class and ID selectors to populate the real-time previews
-		document.querySelector("#template1-output").innerHTML = template1;
-		document.querySelector("#template2-output").innerHTML = template2;
-		document.querySelector("#template3-output").innerHTML = template3;
-	}
-
-	// Call generateSignatures initially to populate with any default values
-	generateSignatures();
+    document.getElementById("template3-output").innerHTML = template3;
+  }
 });
